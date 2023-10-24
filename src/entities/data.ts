83e8714bd3ac7c57cf567009cadc2cd9e829c654
harry.sitohang <harry.sitohang@prakerja.go.id>
2023-10-25 00:27:42 +0700

    update readme

--- src/entities/data.ts
+++ src/entities/data.ts
@@ -37,32 +37,32 @@ export interface ResumeEntity {
 
 
 export const MyResume: ResumeEntity = {
-    name: "Harry Osmar Sitohang",
-    role: "Senior Principle Engineer",
-    profile: "Experienced engineering leader with a proven track record in designing and implementing complex systems across [specific industry or domain]. Adept at driving innovation, managing cross-functional teams, and delivering high-impact solutions. Demonstrated expertise in [key technical skills or technologies], coupled with a deep understanding of industry best practices. Known for optimizing processes, reducing costs, and enhancing overall efficiency. Strong communicator and collaborator with a passion for mentoring and developing talent.",
-    educations: [
+    "name": "Harry Osmar Sitohang",
+    "role": "Senior Principle Engineer",
+    "profile": "Experienced engineering leader with a proven track record in designing and implementing complex systems across [specific industry or domain]. Adept at driving innovation, managing cross-functional teams, and delivering high-impact solutions. Demonstrated expertise in [key technical skills or technologies], coupled with a deep understanding of industry best practices. Known for optimizing processes, reducing costs, and enhancing overall efficiency. Strong communicator and collaborator with a passion for mentoring and developing talent.",
+    "educations": [
         {
-            title: "Bachelor's Degree in Computer Science",
-            university: "BINUS University",
-            year: "2012-2015",
+            "title": "Bachelor's Degree in Computer Science",
+            "university": "BINUS University",
+            "year": "2012-2015"
         },
         {
-            title: "Diploma",
-            university: "Del Polytechnic of Informatics",
-            year: "2007-2010",
-        },
+            "title": "Diploma",
+            "university": "Del Polytechnic of Informatics",
+            "year": "2007-2010"
+        }
     ],
-    languages: [
+    "languages": [
         {
-            name: "Bahasa",
-            progress: 100,
+            "name": "Bahasa",
+            "progress": 100
         },
         {
-            name: "English",
-            progress: 85,
-        },
+            "name": "English",
+            "progress": 85
+        }
     ],
-    experiences: [
+    "experiences": [
         {
             "title": "Senior Principle Engineer",
             "company": "Prakerja",
@@ -70,8 +70,8 @@ export const MyResume: ResumeEntity = {
             "responsibilities": [
                 "Successfully engineered and maintained systems handling a staggering 47 million registered users over the past three years. This involved [briefly describe the key responsibilities and challenges you faced]",
                 "Successfully designed and implemented a microservices architecture that effortlessly scales, ensuring robust performance and easy maintenance, even during periods of rapid growth.",
-                "Led the development of a centralized logging and monitoring solution for microservices, utilizing [mention specific tools, e.g., ELK stack, Prometheus], providing real-time insights into system health and performance.",
-            ],
+                "Led the development of a centralized logging and monitoring solution for microservices, utilizing [mention specific tools, e.g., ELK stack, Prometheus], providing real-time insights into system health and performance."
+            ]
         },
         {
             "title": "Senior Fullstack Engineer",
@@ -79,8 +79,8 @@ export const MyResume: ResumeEntity = {
             "dates": "2018-2019",
             "responsibilities": [
                 "Engineered and managed a high-traffic, multi-server platform catering to a diverse global user base across various timezones. This involved overseeing [briefly describe the key responsibilities and challenges you faced].",
-                "Championed a culture of test-first development, resulting in a unit test coverage of 70% across the codebase and a significant reduction in bug reports and an overall increase in software reliability. This shift led to a [specific metric, e.g., 30% decrease in post-release bug reports].",
-            ],
+                "Championed a culture of test-first development, resulting in a unit test coverage of 70% across the codebase and a significant reduction in bug reports and an overall increase in software reliability. This shift led to a [specific metric, e.g., 30% decrease in post-release bug reports]."
+            ]
         },
         {
             "title": "Technical Lead Engineer",
@@ -89,17 +89,17 @@ export const MyResume: ResumeEntity = {
             "responsibilities": [
                 "Orchestrated the technical infrastructure to accommodate a daily influx of up to 100,000 active users across web, Android, and iOS applications. This entailed [briefly describe the key responsibilities and challenges you faced].",
                 "Established a robust monitoring and alerting system to track key performance indicators, promptly identifying and resolving potential performance bottlenecks.",
-                "Successfully initiated the adoption of Docker in our production environment, revolutionizing our deployment process. This transition led to streamlined, consistent, and efficient deployments, significantly reducing downtime and ensuring a more resilient and agile production environment.",
-            ],
-        },
+                "Successfully initiated the adoption of Docker in our production environment, revolutionizing our deployment process. This transition led to streamlined, consistent, and efficient deployments, significantly reducing downtime and ensuring a more resilient and agile production environment."
+            ]
+        }
     ],
-    expertise: [
+    "expertise": [
         "Microservices Architecture: proficient in designing and implementing microservices architectures, emphasizing scalability, resilience, and maintainability.",
         "DevOps and CI/CD: Deep understanding of DevOps practices and tools. Experienced Implemented robust CI/CD pipelines to automate build, test, and deployment processes.",
         "Monitoring and Alerting: Experienced implemented comprehensive monitoring and alerting solutions systems.",
-        "Team Leadership and Mentoring: Providing technical direction and mentorship. Fostered a culture of technical excellence, knowledge sharing, and continuous learning.",
+        "Team Leadership and Mentoring: Providing technical direction and mentorship. Fostered a culture of technical excellence, knowledge sharing, and continuous learning."
     ],
-    skills: [
+    "skills": [
         "golang",
         "java",
         "php",
@@ -120,9 +120,9 @@ export const MyResume: ResumeEntity = {
         "Jenkins ansible",
         "Design Pattern SOLID Clean Code"
     ],
-    contact: {
-        email: "harryosmarsitohang@gmail.com",
-        phone: "+6281397738684",
-        linkedIn: "https://bit.ly/3FrxiSW",
-    },
+    "contact": {
+        "email": "harryosmarsitohang@gmail.com",
+        "phone": "+6281397738684",
+        "linkedIn": "https://bit.ly/3FrxiSW"
+    }
 };
